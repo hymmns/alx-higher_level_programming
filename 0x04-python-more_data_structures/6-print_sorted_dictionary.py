2065af820c4b2a862c0620eef923f1434f3e3115
hymmns <esholadoye@gmail.com>
2024-02-11 18:57:21 +0100

    fix code error

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,6 +2,5 @@
 
 
 def print_sorted_dictionary(a_dictionary):
-    if type(a_dictionary) != 'NoneType':
         for each_key in sorted(a_dictionary.keys()):
             print("{}: {}".format(each_key, a_dictionary[each_key]))
